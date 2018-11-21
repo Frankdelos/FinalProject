3790bf35fae96ed8010f7e5e989241a50c13362a
JoeHuizar <joehuizar75@gmail.com>
2018-11-20 16:23:02 -0800

    fixed routing -joe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
 
 //angular material
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import{MaterialDesignModule} from '../material-design/material-design.module';
-import {MatRadioModule} from '@angular/material/radio';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from '../material-design/material-design.module';
+import { MatRadioModule } from '@angular/material/radio';
 
 
 //routing material
-import {AppRoutes} from './app.routes';
+import { AppRoutes } from './app.routes';
 import { HomeComponent } from './home/home.component';
 import { PartyInputComponent } from './party-input/party-input.component';
 import { BillTotalComponent } from './bill-total/bill-total.component';
@@ -28,7 +28,7 @@ import { BillTotalComponent } from './bill-total/bill-total.component';
     HomeComponent,
     PartyInputComponent,
     BillTotalComponent,
-    
+
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import {Routes, RouterModule} from '@angular/router';
-import {MainContentComponent} from './main-content/main-content.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
 import { HomeComponent } from './home/home.component';
 import { PartyInputComponent } from './party-input/party-input.component';
 import { BillTotalComponent } from './bill-total/bill-total.component';
 
 const routes: Routes = [
     {
-        path:'',
+        path: '',
         component: HomeComponent,
     },
     {
@@ -14,14 +14,13 @@ const routes: Routes = [
         component: PartyInputComponent,
     },
     {
-        path: '**',
-        component: MainContentComponent,
-    },
-    {
         path: 'bill-total',
         component: BillTotalComponent,
+    },
+    {
+        path: '**',
+        component: MainContentComponent,
     }
-
 ]
 
 
